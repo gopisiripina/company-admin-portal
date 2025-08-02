c75d1e75837314dd4c3e216388eca0afa5fd7ed9
kovvurisuryavenkatareddy <suryavenkatareddy90@gmail.com>
2025-08-02 12:48:25 +0530

    Update API base URL to use HTTPS

--- frontend/src/pages/email/EmailClient.jsx
+++ frontend/src/pages/email/EmailClient.jsx
@@ -226,7 +226,7 @@ useEffect(() => {
 
 
 
-  const API_BASE = 'http://cap.myaccessio.com/api/email';
+  const API_BASE = 'https://cap.myaccessio.com/api/email';
 
   useEffect(() => {
   const handleResize = () => {
